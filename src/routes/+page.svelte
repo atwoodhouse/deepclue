3596d6d680fd9ed9b3053f11d0aecd878250fb2b
Alexander Woodhouse <alexander.woodhouse@hyperlab.se>
2023-04-18 20:26:10 +0200

    Set prettier rules and format project.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { messages, state, type Message } from "./lib/server/stores";
+  import { onMount } from "svelte";
+  import { messages, state, type Message } from "./lib/stores";
 
-    onMount(() => {
-        const message: Message = {"role":"assistant","content":"Room: Library\nVictim: Mrs. Peacock\nWeapon: Candlestick\nText: As you arrive at the castle, you are greeted by James the Butler. He asks for your name and a brief description of your appearance. After a few short pleasantries, he leads you to the Library where you discover the lifeless body of Mrs. Peacock lying on the floor, with a candlestick lying next to her. \n\nQuestions: 20\nPeople: Dr. Orchid, Colonel Mustard, Professor Plum, Miss Scarlet, Mr. Green\nText: All of the characters have been assembled in the room to be questioned. Dr. Orchid is nervously fidgeting with her lab coat, while Colonel Mustard stands with his arms crossed looking defensive. Professor Plum is muttering to himself, while Miss Scarlet is preening her hair, and Mr. Green is pacing back and forth.\n\nWhat would you like to ask first?"};
-        messages.addFromAssistant(message);
-    });
+  onMount(() => {
+    const message: Message = {
+      role: "assistant",
+      content:
+        "Room: Library\nVictim: Mrs. Peacock\nWeapon: Candlestick\nText: As you arrive at the castle, you are greeted by James the Butler. He asks for your name and a brief description of your appearance. After a few short pleasantries, he leads you to the Library where you discover the lifeless body of Mrs. Peacock lying on the floor, with a candlestick lying next to her. \n\nQuestions: 20\nPeople: Dr. Orchid, Colonel Mustard, Professor Plum, Miss Scarlet, Mr. Green\nText: All of the characters have been assembled in the room to be questioned. Dr. Orchid is nervously fidgeting with her lab coat, while Colonel Mustard stands with his arms crossed looking defensive. Professor Plum is muttering to himself, while Miss Scarlet is preening her hair, and Mr. Green is pacing back and forth.\n\nWhat would you like to ask first?",
+    };
+    messages.addFromAssistant(message);
+  });
 </script>
 
 <h1>DeepClue</h1>
@@ -16,21 +20,21 @@
 <p>People talking right now, that should be visualized: {$state.people.join(", ")}</p>
 
 <div class="texts">
-    {#each $state.paragraphs as { who, text }}
-        <p class:you={who === "you"}>{text}</p>
-    {/each}
+  {#each $state.paragraphs as { who, text }}
+    <p class:you={who === "you"}>{text}</p>
+  {/each}
 </div>
 
 <style>
-    :global(html) {
-        background-color: #111;
-        color: #fff;
-    }
-    .texts {
-        background-color: #333;
-        padding: 1rem;
-    }
-    .you {
-        font-weight: 700;
-    }
+  :global(html) {
+    background-color: #111;
+    color: #fff;
+  }
+  .texts {
+    background-color: #333;
+    padding: 1rem;
+  }
+  .you {
+    font-weight: 700;
+  }
 </style>
--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,26 +1,23 @@
-import { Configuration, OpenAIApi } from "openai"
-import { OPENAI_API_KEY } from "$env/static/private"
+import { Configuration, OpenAIApi } from "openai";
+import { OPENAI_API_KEY } from "$env/static/private";
 import { systemPrompt } from "$lib/server/systemPrompt";
 
 export async function POST({ request }) {
-    const configuration = new Configuration({
-        apiKey: OPENAI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
+  const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
 
-    const { messages } = await request.json();
+  const { messages } = await request.json();
 
-    console.log('asking question');
+  console.log("asking question");
 
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-            { "role": "system", "content": systemPrompt },
-            ...messages
-        ]
-    });
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [{ role: "system", content: systemPrompt }, ...messages],
+  });
 
-    console.log(JSON.stringify(completion.data));
+  console.log(JSON.stringify(completion.data));
 
-    return new Response(JSON.stringify(completion.data.choices[0].message));
+  return new Response(JSON.stringify(completion.data.choices[0].message));
 }
