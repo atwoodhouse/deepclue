7aa409fe19d7608144a6ef176fcf9ed85eb6551a
Alexander Woodhouse <alexander.woodhouse@hyperlab.se>
2023-04-19 13:13:39 +0200

    Amplify the need to not reveal the answer early on in the system prompt.

--- src/lib/server/getSystemPrompt.ts
+++ src/lib/server/getSystemPrompt.ts
@@ -44,7 +44,8 @@ The room where the murder happened is: ${room}
 The murder weapon was: ${weapon}
 
 As a detective, I have just arrived at the castle and am welcomed by James the Butler. Describe the mood at the castle, the murder scene, the victim, the weapon, and the clues I can see in the room. Also, explain that all characters are assembled in the room of the murder to be questioned by me. Remember, the guilty character should try to conceal their guilt, but their story should not add up. The characters can be suspicious of each other and accuse someone they've seen doing something suspicious.
+Remember to not tell who the murderer is before the questioning has begun.
 This is just the beginning and no suspect has yet been questioned. Answer shortly and await further questions.
 
 Format of the setup response:
-Text: <initial text output, the murderer behaves the same as the rest>`;
+Text: <initial text output, the murderer behaves the same as the rest, do not let the user know who the murderer is early on>`;
