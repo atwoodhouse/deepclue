2660651249855c12131c51b6dd720d57edf4ccd1
Alexander Woodhouse <alexander.woodhouse@hyperlab.se>
2023-04-21 21:13:37 +0200

    Handle questioning and court stages in API and communicate.

--- src/lib/communicate.ts
+++ src/lib/communicate.ts
@@ -1,27 +1,37 @@
 import { get } from "svelte/store";
-import { messages, type Message, state } from "./stores";
+import { messages, type Message, state, type Stage } from "./stores";
 
-export const communicate = async (messagesToSend: Message[]) => {
+const filterByStage = (messagesToSend: Message[], targetStage: Stage) =>
+  messagesToSend
+    .filter(({ stage }) => stage === targetStage)
+    .map((m) => ({ role: m.role, content: m.content }));
+
+export const communicate = async (messagesToSend: Message[] = []) => {
   state.update((s) => ({ ...s, waitingForAI: true }));
 
-  const { victim, murderer, room, weapon } = get(state);
+  const { victim, murderer, room, weapon, accused } = get(state);
 
   try {
     const res = await fetch("https://deepclue-api.vercel.app/api", {
       method: "POST",
       body: JSON.stringify({
-        messages: messagesToSend,
+        questioning: filterByStage(messagesToSend, 1),
+        court: filterByStage(messagesToSend, 3),
         meta: {
-          victim, murderer, room, weapon
-        }
+          victim,
+          murderer,
+          room,
+          weapon,
+          accused,
+        },
       }),
     });
 
-    if(!res.ok) throw new Error();
+    if (!res.ok) throw new Error();
 
     const newMessage: Message = await res.json();
     messages.addFromAssistant(newMessage);
   } catch {
-    state.update(s => ({ ...s, error: true }));
+    state.update((s) => ({ ...s, error: true }));
   }
 };
--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -8,13 +8,13 @@ export async function POST({ request }) {
   });
   const openai = new OpenAIApi(configuration);
 
-  const { messages, meta } = await request.json();
+  const { questioning, court, meta } = await request.json();
 
   console.log("asking question");
-  
+
   const systemPrompts = [{ role: "system", content: getSystemPrompt(meta) }];
 
-  if(messages.length > 0) {
+  if (questioning.length) {
     systemPrompts.push({
       role: "system",
       content: `As questions are being answered, it shouldn't be hard to figure out who the murderer is. The murderer should try to conceal it, but not very good.
@@ -22,15 +22,33 @@ export async function POST({ request }) {
       Format of the question responses:
       People: <characters names that are talking in this response, as they were defined in the system prompt, comma separated if multiple people are talking in the response>
       Tension: <either "Tense" or "Calm", depending on if this specific text is more or less tense than average in this conversation>
-      Text: <text output, which can contain clues of who the murderer is>`
+      Text: <text output, which can contain clues of who the murderer is>`,
     });
   }
 
-  console.log("messages to send", [...systemPrompts, ...messages]);
+  let messages = [...systemPrompts, ...questioning];
+
+  if (meta.accused) {
+    messages = [
+      ...messages,
+      {
+        role: "system",
+        content: `I, the detective, have now decided that I believe that ${meta.accused} is the murderer.
+      Now we move from the castle into the court room. In the court room I will act as the prosecutor, trying to convice the jury (12 people) that ${meta.accused} actually murdered ${meta.victim}.
+      In the court room there's a judge who always yells "ORDER, ORDER!" who hands over the word to either me or the defendants attorney.
+      The defending attorney is called Ace. He is briefed on everything that was said during the interrogation at the castle and will do his utmost to defend ${meta.accused}.
+      The prosecutor and the attorney Ace take turns, with the prosecutor first being allowed to give his case. They both go two rounds, and then the jury will come with it's verdict.
+      The number of people in the jury of 12 that vote innocent and guily will be presented, and at least 7 people in the jury have to vote guilty in order for the accused to be found guilty and put in prison.`,
+      },
+      ...court,
+    ];
+  }
+
+  console.log("messages to send", messages);
 
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [...systemPrompts, ...messages],
+    messages,
   });
 
   console.log(JSON.stringify(completion.data));
