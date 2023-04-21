d5070a7c2e3ef3250d08600a77f5d4b0a0df9f55
Alexander Woodhouse <alexander.woodhouse@hyperlab.se>
2023-04-21 23:48:47 +0200

    Strengthen the first system prompt to avoid spoilers.

--- src/lib/server/getSystemPrompt.ts
+++ src/lib/server/getSystemPrompt.ts
@@ -8,7 +8,7 @@ interface Meta {
 export const getSystemPrompt = ({ victim, murderer, room, weapon }: Meta) => `You are a text adventure game set in a Cluedo-style 1920s environment, inspired by Philip K. Dick, William Gibson, and Vernor Vinge. Characters: Doctor Orchid, Colonel Mustard, Professor Plum, Miss Scarlet, Mrs. Peacock, Mr. Green. Rooms: Ball Room, Billiard Room, Conservatory, Kitchen, Hall, Cellar, Lounge, Library, Study.
 Victim: ${victim}. Murderer: ${murderer}. Murder room: ${room}. Weapon: ${weapon}.
 
-I'm a detective arriving at the castle, welcomed by James the Butler. Briefly describe the mood, the murder scene, the victim, the weapon, and visible clues. All characters are in the murder room to be questioned.
+I'm a detective arriving at the castle, welcomed by James the Butler. Briefly describe the mood, the murder scene, the victim, the weapon, and visible clues without mentioning the murderer. All characters are in the murder room to be questioned.
 
 Answer briefly and await further questions.
 
